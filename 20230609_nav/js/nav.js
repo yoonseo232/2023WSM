b37630aefb332bd5bdcbf9d7c30c99aa971849c9
yoonseo232 <ys.bang232@gmail.com>
2023-06-20 10:59:03 +0900

    nav js, style.css->nav.css

--- 20230609_nav/js/nav.js
+++ 20230609_nav/js/nav.js
@@ -9,10 +9,15 @@ const navToggleI = nav_toggle.getElementsByTagName("i")[0];
 const navListUl = document.getElementsByClassName("nav-list")[0];
 
 //2. Event Handling
-//햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list"<-> X "bi bi-x-lg"
-//햄버거메뉴 클릭하면, .nav-list에 .show-munu보여주기 <-> 안보여주기
-nav_toggle.onclick = () => alert("안녕");
-nav_toggle.onclick = alert("안녕");
+// nav_toggle.onclick = () => alert("안녕");  //ok
+// nav_toggle.onclick = alert("안녕");     //notok
+nav_toggle.onclick = () => {
+    //햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list"<-> X "bi bi-x-lg"
+     navToggleI.classList.toggle("bi-list"); //toggle => remove <-> add
+     navToggleI.classList.toggle("bi-x-lg");
+    //햄버거메뉴 클릭하면, .nav-list에 .show-munu보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+}
 
 //--------------
 //Html -> JS
