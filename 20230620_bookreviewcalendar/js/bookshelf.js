461e2c822f6edd520b0a177562e9e36741818479
yoonseo232 <ys.bang232@gmail.com>
2023-06-20 12:16:58 +0900

    data, bookshelf

---
+++ 20230620_bookreviewcalendar/js/bookshelf.js
@@ -0,0 +1,11 @@
+//.book-item > img 태그의 src 속성 초기화
+
+//HTML -> JS
+const bookItems = document.getElementsByClassName("book-item");
+let i = 0;
+for(let bookItem of bookItems){
+    const bookItemImg = bookItem.getElementsByTagName("img")[0];  //book-item 하나씩 꺼내어 img 태그 가져오기
+    bookItemImg.src = books[i].img;  //data.js에서 img 값 가져와서 img 태그에 적용
+    console.log(bookItemImg)
+    i++;
+}
---
+++ 20230620_bookreviewcalendar/js/data.js
@@ -0,0 +1,85 @@
+let books = [
+    {
+        "id": 1,
+        "title": "세이노의 가르침",
+        "author": "세이노",
+        "publisher": "세이노출판",
+        "img": "./images/1. 세이노의 가르침.jpg",
+    },
+    {
+        "id": 2,
+        "title": "인생에서 8가지 일에만 집중하라",
+        "author": "인8",
+        "publisher": "인8출판",
+        "img": "./images/2. 인생에서 8가지 일에만 집중하라.jpg",
+    },
+    {
+        "id": 3,
+        "title": "역행자",
+        "author": "행자",
+        "publisher": "행자출판",
+        "img": "./images/3. 역행자.jpg",
+    },
+    {
+        "id": 4,
+        "title": "유연함의 힘",
+        "author": "유연",
+        "publisher": "유연출판",
+        "img": "./images/4. 유연함의 힘.jpg",
+    },
+    {
+        "id": 5,
+        "title": "나는 죽을 때까지 지적이고 싶다",
+        "author": "나죽지",
+        "publisher": "나죽출판",
+        "img": "./images/5. 나는 죽을 때까지 지적이고 싶다.jpg",
+    },
+    {
+        "id": 6,
+        "title": "코드612 누가 어린왕자를 죽였는가",
+        "author": "코드",
+        "publisher": "코드출판",
+        "img": "./images/6. 코드612 누가 어린왕자를 죽였는가.jpg",
+    },
+    {
+        "id": 7,
+        "title": "박태웅의 AI강의",
+        "author": "박태웅",
+        "publisher": "태웅출판",
+        "img": "./images/7. 박태웅의 AI강의.jpg",
+    },
+    {
+        "id": 8,
+        "title": "사장학개론",
+        "author": "장학",
+        "publisher": "장학출판",
+        "img": "./images/8. 사장학개론.jpg",
+    },
+    {
+        "id": 9,
+        "title": "당신은 결국 무엇이든 해내는 사람",
+        "author": "당결",
+        "publisher": "무해출판",
+        "img": "./images/9. 당신은 결국 무엇이든 해내는 사람.jpg",
+    },
+    {
+        "id": 10,
+        "title": "원씽",
+        "author": "씽",
+        "publisher": "씽출판",
+        "img": "./images/10. 원씽.jpg",
+    },
+]
+let reviews = [
+    {
+        "id": 1,
+        "bookId": 1,
+        "userId": 1,
+        "synopsis": "세이노 작가의 인생 가르침",
+        "phrase": "공부 좀 해라",
+        "thoughts": "좋은 말. 지키기는 어려운",
+    },
+]
+
+// console.log(books[0].title);
+// console.log(books[0]["title"]);
