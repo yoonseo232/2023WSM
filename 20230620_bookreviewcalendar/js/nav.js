561577e03d48abba8a408707ed2c427a6ca6953f
yoonseo232 <ys.bang232@gmail.com>
2023-06-20 11:06:52 +0900

    bookreviewcalendar

---
+++ 20230620_bookreviewcalendar/js/nav.js
@@ -0,0 +1,37 @@
+
+//1/ HTML -> js
+//햄버거메뉴 -> navToggleDiv
+//햄버거메뉴 i -> navToggleI
+//.nav-list -> navListUl
+
+// const navToggleDiv = document.getElementById("nav_toggle");
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
+const navListUl = document.getElementsByClassName("nav-list")[0];
+
+//2. Event Handling
+// nav_toggle.onclick = () => alert("안녕");  //ok
+// nav_toggle.onclick = alert("안녕");     //notok
+nav_toggle.onclick = () => {
+    //햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list"<-> X "bi bi-x-lg"
+     navToggleI.classList.toggle("bi-list"); //toggle => remove <-> add
+     navToggleI.classList.toggle("bi-x-lg");
+    //햄버거메뉴 클릭하면, .nav-list에 .show-munu보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+}
+
+//--------------
+//Html -> JS
+//id
+//const navToggleDiv = document.getElementById("nav_toggle");
+//const navToggleDiv = nav_toggle 쓸 수 있지만 JS변수 규칙을 지켜야함
+
+//class
+//const navToggleDiv = document.getElementById("nav-toggle")[0];
+
+//tag
+//const navToggleDiv = get.getElementByTagName("div")[0];
+
+//querySelector, querySelectorAll
+//const navToggleDiv = document.querySelector("#nav_toggle");
+//const navToggleDiv = document.querySelectorAll(".nav_toggle")[0];
+//const navToggleDiv = document.querySelector("div")[0];
