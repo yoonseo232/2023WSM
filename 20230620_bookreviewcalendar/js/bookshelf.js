5ba30dc2815a7cda5587cb2fc1221a1405d7d40d
yoonseo232 <ys.bang232@gmail.com>
2023-06-27 10:27:00 +0900

    .

--- 20230620_bookreviewcalendar/js/bookshelf.js
+++ 20230620_bookreviewcalendar/js/bookshelf.js
@@ -6,6 +6,6 @@ let i = 0;
 for(let bookItem of bookItems){
     const bookItemImg = bookItem.getElementsByTagName("img")[0];  //book-item 하나씩 꺼내어 img 태그 가져오기
     bookItemImg.src = books[i].img;  //data.js에서 img 값 가져와서 img 태그에 적용
-    console.log(bookItemImg)
+    console.log(bookItemImg);
     i++;
 }
